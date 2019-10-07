cb42db4d0f294511e04e392396a3290d0b5a0c43
Shameek Ganguly <shameekg@stanford.edu>
2019-10-07 12:01:58 -0700

    Updates

--- src/daq1.cpp
+++ src/daq1.cpp
@@ -11,6 +11,7 @@ Vector1d speed_sensor_counts;
 Vector1d pressure_sensor_counts;
 Vector1d force_sensor_counts;
 Vector1d torque_sensor_counts;
+Vector1d front_temp_sensor_counts;
 
 // 100 Hz logger
 Logging::Logger logger(10000, "datalog.csv");
@@ -22,6 +23,7 @@ simplecat::Beckhoff_EL9510 bh_el9510;
 simplecat::Beckhoff_EL3356 bh_el3356_force;
 simplecat::Beckhoff_EL3356 bh_el3356_torque;
 simplecat::Beckhoff_EL3062 bh_el3062;
+simplecat::Beckhoff_EL3202 bh_el3202;
 
 #include <signal.h>
 bool rundaq = true;
@@ -35,6 +37,7 @@ void control_callback() {
 	pressure_sensor_counts << bh_el3062.read_data_[0];
 	force_sensor_counts << bh_el3356_force.read_value;
 	torque_sensor_counts << bh_el3356_torque.read_value;
+	front_temp_sensor_counts << bh_el3202.read_data_[0];
 }
 
 int main (int argc, char** argv) {
@@ -42,9 +45,10 @@ int main (int argc, char** argv) {
 	master.addSlave(0, 0, &bh_ek1100);
 	master.addSlave(0, 1, &bh_el3062);
 	master.addSlave(0, 2, &bh_el5152);
-	master.addSlave(0, 3, &bh_el9510);
-	master.addSlave(0, 4, &bh_el3356_force);
-	master.addSlave(0, 5, &bh_el3356_torque);
+	master.addSlave(0, 3, &bh_el3202);
+	master.addSlave(0, 4, &bh_el9510);
+	master.addSlave(0, 5, &bh_el3356_force);
+	master.addSlave(0, 6, &bh_el3356_torque);
 
 	master.setThreadHighPriority();
 	master.activate();
@@ -54,6 +58,7 @@ int main (int argc, char** argv) {
 	logger.addVectorToLog(&pressure_sensor_counts, "pressure");
 	logger.addVectorToLog(&force_sensor_counts, "force");
 	logger.addVectorToLog(&torque_sensor_counts, "torque");
+	logger.addVectorToLog(&front_temp_sensor_counts, "front_temp");
 	logger.start();
 
 	master.run(control_callback, control_freq);
