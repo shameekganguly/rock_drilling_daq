1d7eb98450d46779254c0f47c406d7d228aa2f4d
Shameek Ganguly <shameekg@stanford.edu>
2021-03-11 12:04:15 -0800

    Correct sign for screwjack encoder movement direction

--- src/daq1.cpp
+++ src/daq1.cpp
@@ -89,14 +89,14 @@ double getCalibratedLinearSpeed(double raw_counts) {
 	{
 		linear_speed_last_counts = raw_counts;
 	}
-	linear_speed_raw << (raw_counts - linear_speed_last_counts)*sampling_rate*60/48*2.54/200/4; // cm/min = in/count (1/48)* rev/count (1/200)* cm/in (2.54) * counts/sec * secs/min (60) * beckhoff_counts/count (1/4) (4 fold evaluation)
+	linear_speed_raw << -(raw_counts - linear_speed_last_counts)*sampling_rate*60/48*2.54/200/4; // cm/min = in/count (1/48)* rev/count (1/200)* cm/in (2.54) * counts/sec * secs/min (60) * beckhoff_counts/count (1/4) (4 fold evaluation)
 	linear_speed_filtered = linear_speed_filter.update(linear_speed_raw);
 	linear_speed_last_counts = raw_counts;
 	return linear_speed_filtered[0];
 }
 
 double getCalibratedScrewjackPosition(double raw_counts) {
-	return raw_counts/48*2.54/200/4; // cm/count = in/rev (1/48) * rev/count (1/200) * cm/in (2.54) * beckhoff_counts/count (1/4) (4 fold evaluation)
+	return -raw_counts/48*2.54/200/4; // cm/count = in/rev (1/48) * rev/count (1/200) * cm/in (2.54) * beckhoff_counts/count (1/4) (4 fold evaluation)
 }
 
 double getCalibratedPressure(double raw_counts) {
